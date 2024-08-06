11e81d00c5877d4458e7c9916c92ffc6a74c0240
davidjhovsepian <137075703+davidjhovsepian@users.noreply.github.com>
2024-08-06 10:55:50 +0200

    New code

---
+++ cypher.py
@@ -0,0 +1,15 @@
+def greet(a):
+    print("Hello")
+    print("How do you do")
+    print("Isn't the weather nice?")
+    
+    
+#Functions that allow for inputs
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do{name}")
+    print(f"Isn't the weather nice? {name}")
+
+greet_with_name("Angelo")
+    
